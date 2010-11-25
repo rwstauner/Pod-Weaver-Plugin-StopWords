b0a546aee2a372ab4b6eabbeb458eb179f967f12
Randy Stauner <randy@magnificent-tears.com>
2010-11-24 23:20:11 -0700

    Initial working module.

---
+++ lib/Pod/Weaver/Plugin/StopWords.pm
@@ -0,0 +1,77 @@
+package Pod::Weaver::Plugin::StopWords;
+# ABSTRACT: Dynamically add stopwords to your woven pod
+
+=head1 SYNOPSIS
+
+	# weaver.ini
+	[-StopWords]
+	gather = 1     ; default
+	stopwords = MyExtraWord1 exword2
+
+=cut
+
+use strict;
+use warnings;
+use Moose;
+use Moose::Autobox;
+use namespace::autoclean -also => 'splice_stopwords_from_children';
+
+with 'Pod::Weaver::Role::Finalizer';
+
+has gather => (
+	is      => 'ro',
+	isa     => 'Bool',
+	default => 1
+);
+
+has stopwords => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => '',
+);
+
+sub finalize_document {
+    my ($self, $document, $input) = @_;
+
+	my @stopwords = $self->stopwords;
+
+	if( my $zilla = ($input && $input->{zilla}) ){
+		push(@stopwords, split(/\s+/)) foreach @{ $zilla->{authors} };
+	}
+
+	push(@stopwords, splice_stopwords_from_children($document->children))
+		if $self->gather;
+
+	return unless @stopwords;
+
+    $document->children->unshift(
+        Pod::Elemental::Element::Pod5::Command->new({
+            command => 'for :stopwords',
+            content => join(' ', grep { $_ } @stopwords)
+        }),
+    );
+}
+
+sub splice_stopwords_from_children {
+    my ($children, @stopwords) = @_;
+
+	CHILDREN: foreach my $i ( 0 .. (@$children - 1) ){
+		next unless my $para = $children->[$i];
+		next unless $para->isa('Pod::Elemental::Element::Pod5::Region')
+			and $para->format_name eq 'stopwords';
+
+		push(@stopwords,
+			map { split(/\s+/, $_->content) } $para->children->flatten);
+		splice(@$children, $i, 1);
+
+		redo CHILDREN; # don't increment the counter
+	}
+
+	return @stopwords;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+=for Pod::Coverage finalize_document
